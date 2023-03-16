51a5108bd6c28ba36bbce6fb09e222f00d463fea
Danil <78010930+CupcakewithSnow@users.noreply.github.com>
2023-03-17 01:09:05 +0700

    task:4
    add swagger and user controller

--- d-social-api/src/app.module.ts
+++ d-social-api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
